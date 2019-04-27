055b5e47a1f63e7c4a5c6b2c9b9ba777158d78b7
Hassan Masood <iamhassanmasood@gmail.com>
2019-04-27 05:50:56 +0500

    Hello World

---
+++ Hello World/src/components/App.js
@@ -0,0 +1,20 @@
+import React from "react"
+import './App.css'
+
+function App() {
+    return (
+        <div className='App'>
+            <header>
+                <nav>
+                    <ul>
+                        <li><a href='/home'>Home</a></li>
+                        <li><a href='/about'>About</a></li>
+                    </ul>
+                </nav>
+            </header>
+            <h1>Hello World !!!!</h1>
+        </div>
+    )
+}
+
+export default App
